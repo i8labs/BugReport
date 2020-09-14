fcd0209d624af419549baee5d59d042e514ca0de
khatrironit <ronitkhatri1999@gmail.com>
2020-09-14 21:46:05 +0530

    fixed a bug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import BugReport from './lib/index'
 class App extends Component {
   render(){
     return(
-      <div style={{ width: '100%',height : '100vh', margin: "auto" ,backgroundColor : 'pink'}}>
-      <BugReport user = "ronit" app_id = "test"/>
-    </div>
+      <div >        
+        <div style={{ width: '100%',height : '100vh', backgroundColor : 'pink'}}>    
+        <BugReport user = "ronit" app_id = "test"/>
+        </div>
+      </div>
     )
   }
 }
