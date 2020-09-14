e5a40c69cc53ed608277d86f93f5f4886416f0c0
khatrironit <ronitkhatri1999@gmail.com>
2020-09-14 19:15:19 +0530

    added BugReport Component

---
+++ src/lib/components/BugReport.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import BugIcon from "mdi-react/BugIcon";
+import html2canvas from "html2canvas";
+import Modal from "./Modal";
+import TextArea from "./TextArea";
+import axios from 'axios'
+class BugReport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      screenshot: "",
+      desc: "",
+      isLoading: false,
+      insertedId: "",
+      page_id : window.location.pathname.slice(1)
+    };
+  }
+
+  captureScreenShot = async () => {
+    const canvas = await html2canvas(document.querySelector("body"));
+    const base64 = canvas.toDataURL("image/jpeg");
+    console.log(base64)
+    this.setState({ screenshot: base64, modal: true });
+  };
+
+  handleChange = (e) => {
+    this.setState({ desc: e.target.value });
+  };
+
+  toggle = () =>
+    this.setState({
+      modal: !this.state.modal,
+      screenshot: "",
+      desc: "",
+      insertedId: false,
+      isLoading: false
+    });
+
+  handleSubmit = async (e) => {
+    console.log("handlesubmit")
+    e.preventDefault();
+    try {
+      const entity_id = this.props.user   
+      const app_id = this.props.app_id
+      const { desc, screenshot ,page_id} = this.state;
+
+      const body = { page_id, entity_id, app_id, desc, screenshot };
+      console.log("body report",body)
+      this.setState({ isLoading: true });
+      const res = await axios.post('https://b6skvn9l7h.execute-api.ap-south-1.amazonaws.com/Dev/adminbugreport', body);
+      console.log("bug report",res)
+      const insertedId = res.data.body
+      this.setState({ isLoading: false, insertedId});
+    } catch (e) {
+      console.log(e);
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    const { modal, screenshot, desc, isLoading, insertedId } = this.state;
+    return (
+      <div>
+        <button
+          className="bug-icon"
+          type="button"
+          title="Report a bug"
+          onClick={this.captureScreenShot}
+        >
+          <BugIcon />
+        </button>
+        <Modal open={modal} onCloseClicked={this.toggle} onBackDropClicked={this.toggle}>
+          {insertedId ? (
+            <div>
+              <h5 className="d-flex justify-content-center">
+                Your Problem Ticket No.: <b>{`${insertedId}`}</b>
+              </h5>
+              <div className="d-flex w-100 justify-content-center">
+                <button
+                  onClick={this.toggle}
+                  className="modal_ok"
+                  color="danger"
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          ) : (
+            <form className="form mt-5" onSubmit={this.handleSubmit}>
+              <span className="red">Screenshot</span>
+              <img
+                src={screenshot}
+                alt="screenshot"
+                width="100%"
+                height="380px"
+                className="rounded"
+              />
+              <TextArea
+                doNotAutoResize
+                name="Description"
+                rows="3"
+                className="rounded"
+                placeholder="Describe your issue"
+                autoFocus
+                label="Description"
+                value={desc}
+                onChange={this.handleChange}
+              />
+              <div className="d-flex w-100 justify-content-center">
+                <button
+                  type="reset"
+                  className="modal_cancel"
+                  onClick={this.toggle}
+                  disabled={isLoading}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="modal_ok"
+                  color="danger"
+                  disabled={isLoading}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          )}
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default BugReport;
---
+++ src/lib/components/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Modal.css'
+import BugIcon from "mdi-react/BugIcon";
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+const Modal = (props) => {
+  let ret=null;
+  if (props.open) {
+    ret = (
+      <div className="react-modal-classname">
+      <div className="backdrop" onClick={props.onBackDropClicked}></div>
+      <div className="modal" onClick={props.onModalClicked}> 
+        <div className="close-icon"onClick={props.onCloseClicked}>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="d-flex align-items-center justify-content-center">            
+            <h4 className="text-modal mb-0 modal__title"> <BugIcon /> Report Bug</h4>
+        </div>
+       {props.children} 
+       </div>
+      
+      </div>
+    )
+  }
+  return (<ReactCSSTransitionGroup  transitionName="modal"
+  transitionEnterTimeout={500}
+  transitionLeaveTimeout={300} >{ret}</ReactCSSTransitionGroup>)
+}
+export default Modal;
